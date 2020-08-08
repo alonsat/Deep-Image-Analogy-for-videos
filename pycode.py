43badfb0f86975a870ff1f7c75506b74df6bb32c
alonsat <alonsatchi@gmail.com>
2020-08-08 19:20:55 +0300

    Update pycode.py

--- pycode.py
+++ pycode.py
@@ -75,7 +75,7 @@ for count in range(startframe,NumberOfFrames):
   p7='1'
   p8='3'
   p9='0'
-  p13='/content/deep-image-analogy-for-videos/flows/opticalflow_{}'.format(count)#flowfile
+  p13='/content/Deep-Image-Analogy-for-videos/flows/opticalflow_{}'.format(count)#flowfile
   p15='0.5'#lambda
   if(count==0):
       p10='0'
@@ -190,26 +190,26 @@ for count in range(startframe-1,-1,-1):
       
       image_semantic='{}/frame{}.png'.format(frames_folder_output,count+1)
       recursive_flag=False
-  p1=./deep_image_analogy/source/main.cpp#"C:\\Users\\User\\Downloads\\Deep-Image-Analogy-master\\Deep-Image-Analogy-master\\Build\\x64\\Release\\deep_image_analogy.exe"
-  p2=./deep_image_analogy/modles/#"C:\\Users\\User\\Downloads\\Deep-Image-Analogy-master\\Deep-Image-Analogy-master\\windows\\deep_image_analogy\\models\\"
-  p3='{}\\frame{}.png'.format(output_frames_folder_path,count)
+ p1="/content/Deep-Image-Analogy-for-videos/deep_image_analogy/source/main.cpp"#"C:\\Users\\User\\Downloads\\Deep-Image-Analogy-master\\Deep-Image-Analogy-master\\Build\\x64\\Release\\deep_image_analogy.exe"
+  p2="/content/Deep-Image-Analogy-for-videos/deep_image_analogy/modles/"#"C:\\Users\\User\\Downloads\\Deep-Image-Analogy-master\\Deep-Image-Analogy-master\\windows\\deep_image_analogy\\models\\"
+  p3='{}/frame{}.png'.format(output_frames_folder_path,count)
   p4=image_semantic
   p5=frames_folder_output#"C:\\Users\\User\\Downloads\\Deep-Image-Analogy-master\\Deep-Image-Analogy-master\\Build\\x64\\Release\\"
   p6='0'
   p7='1'
   p8='3'
   p9='0'
-  p13='./flows/opticalflow_{}'.format(count)#flowfile
+  p13='/content/Deep-Image-Analogy-for-videos/flows/opticalflow_{}'.format(count)#flowfile
   p15='0.5'#lambda
   if(count==0):
       p10='0'
       p11='0'
-      p12='1'        
+      p12='3'        
       p14='0'
   else:
-      p10='3'
+      p10='1'
       p11='3'
-      p12='1'
+      p12='3'
       p14='0'
   starttime=time.time()
   if(os.system(p1+" "+p2+" "+p3+" "+p4+" "+p5+" "+p6+" "+p7+" "+p8+" "+p9+" "+p10+" "+p11+" "+p12+" "+p13+" "+p14+" "+p15)):
