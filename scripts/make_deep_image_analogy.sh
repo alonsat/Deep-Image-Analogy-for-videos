02d8e82803f720fb45d9062bbf8e1b96c907a824
alonsat <alonsatchi@gmail.com>
2020-08-08 19:07:35 +0300

    Add files via upload

--- scripts/make_deep_image_analogy.sh
+++ scripts/make_deep_image_analogy.sh
@@ -6,10 +6,12 @@ nvcc $source/*.cpp $source/*.cu -o demo \
 	-std=c++11 \
 	-I./include \
 	-L./build/lib \
+	-L/usr/local/lib \
 	-lopencv_core \
 	-lopencv_highgui \
 	-lopencv_imgproc \
+	-lopencv_imgcodecs \
 	-lboost_system \
 	-lcublas \
 	-lcaffe \
-	-lglog 
+	-lglog
